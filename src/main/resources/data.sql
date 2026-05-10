0fa8776124cdebba46eb62c1eb1a71de455f5d03
aga46435 <aga46435@hs-regensburg.de>
2026-05-11 01:52:27 +0200

    project structure + application yaml

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO apartment (
+    title,
+    description,
+    price,
+    deposit,
+    location,
+    apartment_type,
+    total_occupants,
+    occupants_gender_emoji
+)
+VALUES (
+    'Nice WG near OTH',
+    'Fully furnished room',
+    450,
+    900,
+    'Regensburg',
+    'WG',
+    3,
+    '👩👨👨'
+);
