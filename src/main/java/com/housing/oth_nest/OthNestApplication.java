4220054f66a680301f35b499ac34a7432a707aa3
aga46435 <aga46435@hs-regensburg.de>
2026-05-11 01:39:02 +0200

    Initial commit

---
+++ src/main/java/com/housing/oth_nest/OthNestApplication.java
@@ -0,0 +1,13 @@
+package com.housing.oth_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OthNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OthNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/housing/oth_nest/OthNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housing.oth_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OthNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
